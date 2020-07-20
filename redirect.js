6f2b0b269c8486f88235e1d45d6c1c1c523353f4
Cody J. King <codytheking93@gmail.com>
2020-07-20 08:20:38 -0700

    Clean up code

--- redirect.js
+++ redirect.js
@@ -1,8 +1,10 @@
+// Create URL object
 var url = new URL(window.location.href);
-// var new_url = url.protocol + "//smile.amazon.com" + url.pathname;
 
+// Construct new URL
 var host_loc = url.toString().indexOf(url.host);
 var after_host = url.toString().substring(host_loc + url.host.length + 1);
 var new_url = url.protocol + "//smile.amazon.com/" + after_host;
 
+// Redirect to page on Amazon Smile
 window.location.href = new_url;
